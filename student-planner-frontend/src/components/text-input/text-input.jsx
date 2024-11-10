ce5f37ee01c721764f22c57ce5c20602c36d129e
Bailey Licht <baileymlicht@gmail.com>
2024-11-10 17:05:04 -0600

    Toggle visibility on password inputs

--- student-planner-frontend/src/components/text-input/text-input.jsx
+++ student-planner-frontend/src/components/text-input/text-input.jsx
@@ -1,4 +1,9 @@
 import styles from './text-input.module.css';
+import {
+  MdOutlineVisibility as Visible,
+  MdOutlineVisibilityOff as Hidden,
+} from 'react-icons/md';
+import { useCallback, useState } from 'react';
 
 export const TextInput = props => {
   const {
@@ -10,16 +15,30 @@ export const TextInput = props => {
     isPassword = false,
     className,
   } = props;
+  const [visible, setVisible] = useState(false);
+  const toggleVisibility = useCallback(() => {
+    setVisible(previous => !previous);
+  }, [setVisible]);
+
+  const showText = !isPassword || visible;
 
   return (
     <div className={`${styles.textInput} ${className}`}>
       <label className={styles.textInputLabel}>{label}</label>
-      <input
-        type={isPassword ? 'password' : 'text'}
-        value={value}
-        onChange={e => onChange(e.target.value)}
-        className={`${styles.input} ${isError && styles.inputError}`}
-      />
+      <div className={styles.inputWrapper}>
+        <input
+          type={showText ? 'text' : 'password'}
+          value={value}
+          onChange={e => onChange(e.target.value)}
+          className={`${styles.input} ${isError && styles.inputError}`}
+        />
+        <div
+          className={`${styles.iconWrapper} ${!isPassword && styles.hidden}`}
+          onClick={toggleVisibility}
+        >
+          {visible ? <Hidden /> : <Visible />}
+        </div>
+      </div>
       {isError && errorText && (
         <div className={styles.errorLabel}>{errorText}</div>
       )}
