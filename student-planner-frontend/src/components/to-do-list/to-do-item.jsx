0435a6fe794377abe23f23d12ba081dfa73dac89
Bailey Licht <baileymlicht@gmail.com>
2024-12-06 16:40:52 -0600

    Add functionality for agenda item checkbox

--- student-planner-frontend/src/components/to-do-list/to-do-item.jsx
+++ student-planner-frontend/src/components/to-do-list/to-do-item.jsx
@@ -1,11 +1,14 @@
 import { CiEdit } from 'react-icons/ci';
 import styles from './to-do-item.module.css';
-import { useModalContext } from '../../hooks';
+import { useModalContext, useUpdate } from '../../hooks';
 import { useCallback } from 'react';
+import { SquareLoader } from 'react-spinners';
 
 export const ToDoItem = ({ item }) => {
   const { name, complete, dueDate } = item;
   const { openEditModal } = useModalContext();
+  const updateMutation = useUpdate(item.type, () => {});
+
   const onClickEdit = useCallback(() => {
     const subject = item.subject?.id;
     const dueDate = new Date(Date.parse(item.dueDate));
@@ -13,13 +16,30 @@ export const ToDoItem = ({ item }) => {
     openEditModal(item.type, itemToEdit);
   }, [item, openEditModal]);
 
+  const onClickCheckbox = useCallback(() => {
+    const itemBody = {
+      ...item,
+      dueDate: new Date(Date.parse(item.dueDate)),
+      complete: !item.complete,
+      subject: item.subject?.id,
+    };
+    updateMutation.mutate(itemBody);
+  }, [item, updateMutation]);
+
   return (
     <div className={styles.agendaItem}>
-      <input
-        className={styles.agendaItemCheckbox}
-        type='checkbox'
-        checked={complete}
-      />
+      {updateMutation.isLoading ? (
+        <div className={styles.loadingWrapper}>
+          <SquareLoader className={styles.loading} />
+        </div>
+      ) : (
+        <input
+          className={styles.agendaItemCheckbox}
+          type='checkbox'
+          checked={complete}
+          onChange={onClickCheckbox}
+        />
+      )}
       <h4>{name}</h4>
       <h4>{dueDate}</h4>
       <div className={styles.editButtonWrapper}>
