091eebdfda5e3e05a7ab4d810aa4c6179cbc5d60
Bailey Licht <baileymlicht@gmail.com>
2024-12-08 09:59:07 -0600

    Prep for deployment

---
+++ student-planner-backend/src/main/kotlin/baileylicht/backend/configuration/StartupEventListener.kt
@@ -0,0 +1,31 @@
+package baileylicht.backend.configuration
+
+import baileylicht.backend.models.UserEntity
+import baileylicht.backend.repositories.UserRepository
+import baileylicht.backend.security.Role
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.event.EventListener
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Component
+
+@Component
+class StartupEventListener(
+    @Autowired private val userRepository: UserRepository,
+    @Autowired private val passwordEncoder: PasswordEncoder
+) {
+    @Value("\${studentplanner.admin.username}")
+    private lateinit var adminUsername: String
+
+    @Value("\${studentplanner.admin.password}")
+    private lateinit var adminPassword: String
+
+
+    @EventListener
+    fun appReady(event: ApplicationReadyEvent) {
+        val encodedPassword = passwordEncoder.encode(adminPassword)
+        val user = UserEntity(username = adminUsername, password = encodedPassword, role = Role.ADMIN)
+        userRepository.save(user)
+    }
+}
